5bba89a330eb4b58e8331e033f2765c1c30e2079
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-04-26 00:39:27 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,4 +1,10 @@
-  int num,flag=0;
+// check whether the given number is prime number or not
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+int num,flag=0;
     cin>>num;
     for(int i=2;i<num/2;i++){
         if(num%i==0){
@@ -11,4 +17,5 @@
     }
     else{
         cout<<"not prime";
+      return 0;
     }
