ed3823ec4aee381e24c6214871562cb8b8f04064
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-04-27 00:58:45 +0530

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+//************program to check even odd*****************
+#include <iostream>
+using namespace std;
+int main(){   
+int num;
+    cout<<"enter your number: ";
+    cin>>num;
+
+    using if else 
+    if(num%2==0){
+        cout<<"number "<<num<<" is even no."<<endl;
+    }
+    else{
+        cout<<"entered number is odd no.";
+    }
+return 0;
+}
