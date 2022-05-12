08f8c1786af36c0ffd268895ee77a980b99f4394
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-05-12 18:00:10 +0530

    Create pattern_1.cpp

---
+++ pattern_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() { 
+int num, i,j;                                    
+    cin>>num;
+    for(i=1;i<=num;i++){
+        for ( j=1;j<=num; j++)
+        {
+         cout<<"* ";
+        }
+        cout<<endl;
+    }
+  return 0;
+}
+//  OUTPUT : 
+//  For num =5
+
+* * * * *   
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
