2a2e56f4a551dba4c05e63a2677503034a503c4e
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-05-14 15:11:16 +0530

    Update pattern_1.cpp

--- pattern_1.cpp
+++ pattern_1.cpp
@@ -21,3 +21,26 @@ int num, i,j;
 * * * * * 
 * * * * * 
 * * * * * 
+// ************************* pattern question 2 ****************************    
+#include<iostream>
+using namespace std;
+int main() {  
+    int num,i,j;
+    cin>>num;
+    for(i=1;i<=num;i++){
+        for ( j=1;j<=i; j++)
+        {
+         cout<<"* ";
+        }
+        cout<<endl;
+    }
+return 0;
+}
+//  OUTPUT :-
+//  For num =5
+
+* 
+* *
+* * * 
+* * * * 
+* * * * *  
