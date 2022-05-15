9e3c1c79c36501d10425ad7d09f23da9ed1dbfbb
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-05-16 00:19:32 +0530

    Update pattern_2.cpp

--- pattern_2.cpp
+++ pattern_2.cpp
@@ -20,3 +20,26 @@ return 0;
 * * *
 * *
 * 
+// **************pattern*********************
+#include<iostream>
+using namespace std;
+int main() { 
+      int rows,i,j;
+    cin>>rows;
+    for(i=1;i<=rows;i++){
+        for ( j=1;j<=i; j++)
+        {
+         cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// OUTPUT  -->
+// for rows = 5
+1 
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
