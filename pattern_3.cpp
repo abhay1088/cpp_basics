090ab11f7977fb5b2f48eacc3317beaf27fb7f7c
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2022-05-18 17:30:19 +0530

    Update pattern_3.cpp

--- pattern_3.cpp
+++ pattern_3.cpp
@@ -21,3 +21,29 @@ int rows,i,j;
 1 2 3
 1 2
 1
+
+// ****************** another pattern **************************
+#include<iostream>
+using namespace std;
+int main() {    
+       int rows,i,j,k;
+    cin>>rows;
+    for(i=1;i<=rows;i++){
+        for(j=i;j<=rows ;j++){
+            cout<<" ";
+        }
+        for(k=1;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+//  OUTPUT :
+//  FOR ROWS =5
+     *
+    **
+   ***
+  ****
+ *****
